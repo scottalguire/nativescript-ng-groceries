fb8993e4ef94efe3480b48c237e9a907da3a3bca
Scott <scott.alguire@gmail.com>
2017-06-03 21:54:41 -0400

    Completed 3.5:Routing

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,50 +1,7 @@
 import { Component } from "@angular/core";
-import { User } from "./shared/user/user";
-import { UserService } from "./shared/user/user.service";
 
 @Component({
-  selector: "my-app",
-  providers: [UserService],
-  templateUrl: 'pages/login/login.html',
-  styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
+  selector: "main",
+  template: "<page-router-outlet></page-router-outlet>"
 })
-export class AppComponent {
-  user: User;
-  isLoggingIn = true;
-
-  constructor(private userService: UserService) {
-    this.user = new User();
-    //hardcode my user credentials for development purposes
-    this.user.email = "scott@groceries.com";
-    this.user.password = "gr0ceries";
-  }
-
-  submit() {
-    //alert("You're using: " + this.user.email);
-    console.log(`hello ${new Date()}`);
-
-    if (this.isLoggingIn) {
-      this.login();
-    } else {
-      this.signUp();
-    }
-  }
-  login() {
-    //todo
-  }
-  signUp() {
-    this.userService.register(this.user)
-      .subscribe(
-        () => {
-          alert("Your account was successfully created.");
-          this.toggleDisplay();
-        },
-        () => alert("Unfortunately we were unable to create your account.")
-      );
-  }
-
-  toggleDisplay() {
-    this.isLoggingIn = !this.isLoggingIn;
-  }
-
-}
+export class AppComponent { }
---
+++ app/pages/login/login.component.ts
@@ -0,0 +1,55 @@
+import { Component } from "@angular/core";
+import { User } from "../../shared/user/user";
+import { UserService } from "../../shared/user/user.service";
+import { Router } from "@angular/router";
+
+@Component({
+  selector: "my-app",
+  providers: [UserService],
+  templateUrl: 'pages/login/login.html',
+  styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
+})
+export class LoginComponent {
+  user: User;
+  isLoggingIn = true;
+
+  constructor(private router: Router, private userService: UserService) {
+    this.user = new User();
+    //hardcode my user credentials for development purposes
+    this.user.email = "scott@groceries.com";
+    this.user.password = "gr0ceries";
+  }
+
+  submit() {
+    //alert("You're using: " + this.user.email);
+    console.log(`hello ${new Date()}`);
+
+    if (this.isLoggingIn) {
+      this.login();
+    } else {
+      this.signUp();
+    }
+  }
+  login() {
+    this.userService.login(this.user)
+      .subscribe(
+        () => this.router.navigate(["/list"]),
+        (error) => alert("Unfortunately we could not find your account.")
+      );
+  }
+  signUp() {
+    this.userService.register(this.user)
+      .subscribe(
+        () => {
+          alert("Your account was successfully created.");
+          this.toggleDisplay();
+        },
+        () => alert("Unfortunately we were unable to create your account.")
+      );
+  }
+
+  toggleDisplay() {
+    this.isLoggingIn = !this.isLoggingIn;
+  }
+
+}
