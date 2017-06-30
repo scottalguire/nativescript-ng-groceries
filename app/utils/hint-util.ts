a46f97c876bfaf0d2cb42b366b1547529e36a782
Scott <scott.alguire@gmail.com>
2017-06-29 22:56:41 -0400

    6.2 Accessing Android APIs complete

--- app/utils/hint-util.ts
+++ app/utils/hint-util.ts
@@ -1,15 +1,19 @@
 import { Color } from "color";
 import { TextField } from "ui/text-field";
 
-decalre var NSAttributedString: any;
+declare var NSAttributedString: any;
 declare var NSDictionary: any;
 declare var NSForegroundColorAttributeName: any;
 
 export function setHintColor(args: { view: TextField, color: Color }) {
-    let dictionary = new NSDictionary(
-        [args.color.ios],
-        [NSForegroundColorAttributeName]
-    );
-    args.view.ios.attributePlaceholder = NSAttributedString.alloc().initWithStringAttributes(
-        args.view);
+    if (args.view.android) {
+        args.view.android.setHintTextColor(args.color.android);
+    }
+    if (args.view.ios) {
+        let dictionary = new NSDictionary(
+            [args.color.ios],
+            [NSForegroundColorAttributeName]
+        );
+        args.view.ios.attributePlaceholder = NSAttributedString.alloc().initWithStringAttributes(args.view.hint, dictionary);
+    }
 }
